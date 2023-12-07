c441e6ab38d5f3d6cf798d11201d030755b5c51b
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2023-12-07 11:04:11 +0700

    Feat: Restart Handler

--- dependencies/routes.go
+++ dependencies/routes.go
@@ -2,6 +2,7 @@ package dependencies
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +16,7 @@ func SetupRoutes(router *gin.Engine) {
 	// router.GET("/ws/bid", gin.WrapF(http.HandlerFunc(BidScore(h))))
 	router.POST("/run", RunHandler)
 	router.POST("/terminate", TerminateHandler)
+	router.POST("/restart", RestartHandler)
 
 }
 
@@ -85,4 +87,49 @@ func TerminateHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Terminating " + ws})
 
+}
+
+func RestartHandler(c *gin.Context) {
+	// Get path from request body
+	var body RequestBody
+	// Bind the JSON request body to the struct
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ws := body.Ws
+	wsPath, err := ReadWs(ws)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Check if exe is already terminated or not, if not just run the command
+	_, ok := RunningExes[wsPath]
+	if !ok {
+		err = RunningExe(wsPath, ws)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Running " + ws})
+		return
+	}
+
+	err = TerminateExe(wsPath)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	err = RunningExe(wsPath, ws)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Restarting " + ws})
 }
